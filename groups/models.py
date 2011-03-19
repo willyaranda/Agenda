4688fc818e22e49a71422a45b08a9fe47bfd6be0
Guillermo López <willyaranda@gmail.com>
2011-03-19 23:26:37 +0100

    Adding groups code

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    users = models.ManyToManyField(User)
